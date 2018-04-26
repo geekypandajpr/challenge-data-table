13f6682005b68f745a6ad35c8234ec299138dc5a
Sumit Sharma <sumitsharma88@users.noreply.github.com>
2018-04-26 11:32:45 +0530

    Update report.jsx
    Update for Task for grouping.

--- report.jsx
+++ report.jsx
@@ -1,11 +1,75 @@
-var React = require('react')
-var ReactPivot = require('react-pivot')
-var createReactClass = require('create-react-class')
+var React = require('react');
+var ReactPivot = require('react-pivot');
+var createReactClass = require('create-react-class');
 
-var rows = require('./data.json')
+var rows = require('./data.json');
+var dimensions = [
+  {value: 'date', title: 'Date'},
+  {value: 'host', title: 'Host'}
+];
+
+var reduce = function(row, memo) {
+  memo.load = (memo.load || 0) + (row.type == 'load' ? 1 : 0);
+  memo.display = (memo.display || 0) + (row.type == 'display' ? 1 : 0);
+  memo.impression = (memo.impression || 0) + (row.type == 'impression' ? 1 : 0); 
+  return memo;
+};
+
+var calculations = [
+    {
+      title: 'Impressions',
+      value: function(row) {
+          return row.impression;
+      },
+      className: 'alignRight'
+    },
+    {
+      title: 'Loads',
+      value: function(row) {
+        return row.load;
+      },
+      className: 'alignRight'
+    },
+    {
+      title: 'Display',
+      value: function(row) {
+        return row.display;
+      },
+      className: 'alignRight'
+    },
+    {
+      title: 'LoadRate',
+      value: function(row) {
+        return row.load / row.impression;
+      },
+      template: function(val, row) {
+          return val.toFixed(2) + '%';
+      },
+      className: 'alignRight'
+    },
+    {
+      title: 'DisplayRate',
+      value: function(row) {
+        return row.display / row.load;
+      },
+      template: function(val, row) {
+          return val.toFixed(2) + '%';
+      },
+      className: 'alignRight'
+    }
+];
 
 module.exports = createReactClass({
-  render () {
-    return <div>Report</div>
-  }
+    render () {
+        return (
+          <div>
+              <div>Report</div>
+              <ReactPivot rows={rows}
+                dimensions  ={dimensions}
+                reduce={reduce}
+                activeDimensions={['Date','Host']}
+                calculations={calculations} />
+          </div> 
+        );
+    }
 })
